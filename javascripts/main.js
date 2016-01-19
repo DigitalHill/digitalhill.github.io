155f79e71fddfa7c7da3ee4a4bbbbed3cc794f10
MMernitz <marcelmernitz@googlemail.com>
2016-01-19 09:48:39 +0100

    Removed unnecessary code.
    Removed code that became obsolete by the last deletion.

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@ console.log('This would be the main JS file.');
 $(document).ready(function() {
 	$('#colorpen').css('cursor', 'pointer');
 	$('#colorpen').click(function() { //by clicking on the pencil the marked up words shall be turned into red or green
-		$('.markred').toggleClass('red'), $('.markgreen').toggleClass('green'); //clicking it again will remove the class
+		$('.markred').toggleClass('red'); //clicking it again will remove the class
 	});
 });
 
